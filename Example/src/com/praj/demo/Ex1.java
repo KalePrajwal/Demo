8b034f502358e085be4168babe8fc573bd5845b5
Prajwal <prajwalkale0012@gmail.com>
2023-02-06 15:51:34 +0530

    Added Array List

---
+++ Example/src/com/praj/demo/Ex1.java
@@ -0,0 +1,15 @@
+package com.praj.demo;
+
+import java.util.ArrayList;
+
+public class Ex1 {
+
+	public static void main(String[] args) 
+	{
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		al.add(100);
+		al.add(200);
+		System.out.println(al);
+	}
+
+}
